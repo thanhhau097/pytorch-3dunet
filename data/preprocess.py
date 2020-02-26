eb70681d5345835bdda02219cd90edaad6e11b85
Nguyen Thanh Hau <thanhhau097@gmail.com>
2020-02-27 08:04:22 +1100

    update config

--- data/preprocess.py
+++ data/preprocess.py
@@ -23,10 +23,10 @@ def read_sub_type(root, image_type='HGG'):
         for tail in raw_tails:
             path = os.path.join(root, image_type, filename, filename + tail)
             img = nib.load(path)
-            raw.append(img.get_fdata().astype(np.uint8)) # TODO: convert to int8
+            raw.append(img.get_data().astype(np.uint8)) # TODO: convert to int8
 
         raw = np.array(raw)
-        label = nib.load(os.path.join(root, image_type, filename, filename + label_tail)).get_fdata().astype(np.uint8)
+        label = nib.load(os.path.join(root, image_type, filename, filename + label_tail)).get_data().astype(np.uint8)
 
         folder = os.path.join(root, 'h5', image_type)
         if not os.path.exists(folder):
@@ -40,9 +40,25 @@ def read_data(root):
         read_sub_type(root, image_type)
 
 
+def list_files(root, kind='train', output='all.txt'):
+    files = []
+    if kind == 'train':
+        subs = ['HGG', 'LGG']
+        for sub in subs:
+            files += [os.path.join(sub, file) for file in os.listdir(os.path.join(root, sub))]
+    else:
+        files = [file for file in os.listdir(root)]
+
+    with open(os.path.join(root, output), 'w') as f:
+        for file in files:
+            f.write(file)
+            f.write('\n')
+
 def main():
     root = '2018/MICCAI_BraTS_2018_Data_Training'
-    read_data(root)
+    list_files('2018/MICCAI_BraTS_2018_Data_Training')
+    list_files('2018/MICCAI_BraTS_2018_Data_Validation', kind='val', output='val.txt')
+
 
 
 if __name__ == '__main__':
